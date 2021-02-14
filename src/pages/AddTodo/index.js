b4ee0efaf54833d94c840a7e2413215418b3ecf4
quixote15 <santos2tiago@gmail.com>
2021-02-14 13:01:15 -0300

    feature: add todo page

--- src/pages/AddTodo/index.js
+++ src/pages/AddTodo/index.js
@@ -1,14 +1,88 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {
+  Container,
+  Header,
+  Button,
+  Title,
+  Body,
+  CheckBox,
+  Content,
+  Form,
+  Item,
+  Input,
+  ListItem,
+} from 'native-base';
 // import { Container } from './styles';
 
-const AddTodo = () => {
+const CATEGORY = Object.freeze({
+  WORK: 'work',
+  SHOP: 'shopping',
+  LEARN: 'learning',
+});
+
+const AddTodo = ({navigation}) => {
+  const [category, setCategory] = useState('');
+
+  function toggleCategory(selectedCategory) {
+    setCategory(selectedCategory);
+  }
+
+  function handleSubmit() {
+    // TODO: implement this method.
+    navigation.goBack();
+  }
   return (
-    <View>
-      <Text>Add Todo Page</Text>
-    </View>
+    <Container>
+      <Content>
+        <Form>
+          <Item>
+            <Input placeholder="Task Name" last />
+          </Item>
+
+          <Title style={styles.titleStyle}>Choose Category</Title>
+          <ListItem>
+            <CheckBox
+              checked={category === CATEGORY.SHOP}
+              onPress={() => toggleCategory(CATEGORY.SHOP)}
+            />
+            <Body>
+              <Text style={styles.checkboxTitle}>Shopping</Text>
+            </Body>
+
+            <CheckBox
+              checked={category === CATEGORY.WORK}
+              onPress={() => toggleCategory(CATEGORY.WORK)}
+            />
+            <Body>
+              <Text style={styles.checkboxTitle}>Work</Text>
+            </Body>
+
+            <CheckBox
+              checked={category === CATEGORY.LEARN}
+              onPress={() => toggleCategory(CATEGORY.LEARN)}
+            />
+            <Body>
+              <Text style={styles.checkboxTitle}>Learning</Text>
+            </Body>
+          </ListItem>
+
+          <Button full onPress={handleSubmit}>
+            <Text>Create Task</Text>
+          </Button>
+        </Form>
+      </Content>
+    </Container>
   );
 };
 
 export default AddTodo;
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    marginTop: 10,
+  },
+  checkboxTitle: {
+    marginLeft: 5,
+  },
+});
